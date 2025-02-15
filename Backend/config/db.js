93e2b71e734f5322e1bb5e7343f7f59f3bff3e4a
Ayush Singh <124523646+ayushsingh186312@users.noreply.github.com>
2025-02-15 20:53:23 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:186312@cluster0.ovanjzw.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mon=del').then(()=>console.log("DB Connected"));
 }
